7badcfad1ad98195798a73f83f6c02143959656a
balta <baltaasanchez@gmail.com>
2024-08-29 23:42:54 -0300

    proyecto finalizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using POO_TP;
+using System;
+using System.Collections.Generic;
+
+namespace EstacionMeteorologicaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear una instancia de la Estación Meteorológica
+            EstacionMeteorologica estacion = new EstacionMeteorologica();
+            estacion.VerTemperaturas(2, 1); //VER TEMPERATURAS DE UN DÍA EN ESPECIFICO
+
+            double promedio = estacion.verPromedioTotal();
+            Console.WriteLine($"Promedio total de temperaturas: {promedio}°C"); // VER EL PROMEDIO MENSUAL DE TEMPERATURAS
+
+
+            double promedioSemanal = estacion.verPromedioSemanal(2);
+            Console.WriteLine($"El promedio de la semana que indicaste es de {promedioSemanal}°C");
+        }
+    }
+}
